8221bfe5d040d0dc1852420184271c34fdd4573a
isw4 <isw4@pitt.edu>
2017-10-26 00:27:07 -0700

    initial update

--- coffee/flask_coffee.py
+++ coffee/flask_coffee.py
@@ -9,3 +9,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+if __name__ == "__main__":
+    print("Opening for global access on port {}".format(CONFIG.PORT))
+    app.run(port=8000, host="0.0.0.0")
