26680362373a0f93f38238f7d2ab42878e2623c3
isw4 <isw4@pitt.edu>
2017-10-26 00:39:32 -0700

    initial update

--- coffee/flask_coffee.py
+++ coffee/flask_coffee.py
@@ -3,13 +3,27 @@ DOCUMENTATION HERE
 """
 
 from flask import Flask, url_for, render_template
+import config
+import logging
 
+###
+# Globals
+###
 app = Flask(__name__)
+CONFIG = config.configuration()
+app.secret_key = CONFIG.SECRET_KEY
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+#############
+
+app.debug = CONFIG.DEBUG
+if app.debug:
+    app.logger.setLevel(logging.DEBUG)
+
 if __name__ == "__main__":
-    print("Opening for global access on port {}".format(8000))
-    app.run(port=8000, host="0.0.0.0")
+    print("Opening for global access on port {CONFIG.PORT}".format(8000))
+    app.run(port=CONFIG.PORT, host="0.0.0.0")
